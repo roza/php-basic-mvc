44b9a678760924723b084a4329cc9ac7462e7be9
Gérard Rozsavolgyi <roza@univ-orleans.fr>
2017-02-01 12:24:32 +0100

    mise en place formulaire detail

--- templates/t-detail.php
+++ templates/t-detail.php
@@ -1,9 +1,21 @@
 <?php
-$title="Recherche de la personne d'ID".$ami->ID;
+$title="Personne d'ID".$ami->ID;
 ob_start();
-// on démarre la bufferisation pour $content
-echo $ami->NOM.'<br>';
-echo $ami->PRENOM.'<br>';
+?>
+<form id="fdetail" action="controleur.php">
+  <input type="hidden" name="action" value="patch">
+  <input type="hidden" name="id" value="<?php echo $ami->ID ?>">
+  <p> NOM:
+  <input type="text" value="<?php echo $ami->NOM ?>">
+  <p> PRENOM:
+  <input type="text" value="<?php echo $ami->PRENOM ?>">
+  <p> NAISSANCE:
+  <input type="text" name="naissance" value="<?php echo $ami->NAISSANCE ?>">
+  <p> VILLE:
+  <input type="text" name="ville" value="<?php echo $ami->VILLE ?>">
+  <input type="submit" value="modifier">
+</form>
+<?php
 // et on récupère le contenu dans $content
 $content = ob_get_clean();
 // avant de tout afficher
